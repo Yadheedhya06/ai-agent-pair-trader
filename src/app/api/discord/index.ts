00953b17cf0072e87afa360e32fe29480c86562e
Yadheedhya <yadheedhya6@gmail.com>
2025-01-12 22:46:50 +0530

    added randomStrength

--- src/app/api/discord/index.ts
+++ src/app/api/discord/index.ts
@@ -4,8 +4,15 @@ import { CorrelationStrength } from "../../../../public/types/correlation";
 async function main() {
   const startTime = performance.now();
   
+  const correlationStrengths = [
+    CorrelationStrength.VERY_STRONG,
+    CorrelationStrength.STRONG,
+    CorrelationStrength.MODERATE
+  ];
+  const randomStrength = correlationStrengths[Math.floor(Math.random() * correlationStrengths.length)];
+  
   const db = new ApplicationDatabase()
-  const correlation = await db.getRandomCorrelationByCategory(CorrelationStrength.STRONG)
+  const correlation = await db.getRandomCorrelationByCategory(randomStrength)
   console.log(correlation)
   
   const endTime = performance.now();
