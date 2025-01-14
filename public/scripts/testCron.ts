eefc1387a038030acd750a8506b0f8faad291759
Yadheedhya <yadheedhya6@gmail.com>
2025-01-15 03:31:03 +0530

    fix dep

--- public/scripts/testCron.ts
+++
@@ -1,9 +0,0 @@
-import { alertDiscord } from "@/app/api/cron/discord/route";
-
-(async () => {
-  try {
-    await alertDiscord();
-  } catch (error) {
-    console.error('Error running alertDiscord:', error);
-  }
-})();
